23c52bee4dd512f8d041599d3ff101fbe20a638c
Articha <tima-1324@mail.ru>
2023-12-09 15:20:51 +0300

    Changed CheckHook.__call__ signature

--- src/kipygen/checkhooks.py
+++ src/kipygen/checkhooks.py
@@ -1,5 +1,4 @@
-from typing import Any
-
+from typing import Any, Union
 
 __all__ = (
     'CheckHook',
@@ -18,20 +17,25 @@ class CheckHook:
     ...    def __init__(self, value: Any):
     ...        self.value = value
     ...
-    ...    def __call__(self, function: callable, *args, **kwargs) -> Any:
-    ...        arguments = list(args)
-    ...        # Finding self in arguments, because this hook are passing to func
-    ...        index = arguments.index(self)
-    ...        if index != -1:
-    ...            # Found? Replace with real value
-    ...            arguments[index] = self.value
-    ...        # Call function as nothing happened
-    ...        return function(*arguments, **kwargs)
+    ...    def __call__(
+    ...        self,
+    ...        function: callable,
+    ...        target_method: callable,
+    ...        send_value: Any,
+    ...        timeout: Union[int, float]
+    ...    ) -> Any:
+    ...        return function(target_method, self.value, timeout)
     This class simply do nothing. Example of ValueTuple.send with and without:
     without: [1, 2, 3]
     with: [1, Forward(2), 3]
     """
     __slots__ = ()
 
-    def __call__(self, function: callable, *args, **kwargs) -> Any:
-        return function(*args, **kwargs)
+    def __call__(
+        self,
+        function: callable,
+        target_method: callable,
+        send_value: Any,
+        timeout: Union[int, float]
+    ) -> Any:
+        return function(target_method, send_value, timeout)
